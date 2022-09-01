f6b73ee4581e9e918139041d32d3daa3cc2b3e02
Daniil Gorbunov <17042755+daniil-gorbunov@users.noreply.github.com>
2022-09-01 11:20:42 -0700

    kadane's-algorithm solutions added

---
+++ algo-expert/medium/kadane's_algorithm/1.py
@@ -0,0 +1,15 @@
+# medium, famous algorithms
+# time O(n) | space O(1)
+def kadanesAlgorithm(array):
+    start = 0
+    while start < len(array) - 1:
+        if array[start] >= 0 or array[start] > array[start + 1]:
+            break;
+        start += 1
+    sum = array[start]
+    maxSum = sum
+    for i in range(start + 1, len(array)):
+        sum += array[i]
+        maxSum = max(maxSum, sum)
+        sum = max(sum, 0)
+    return maxSum
