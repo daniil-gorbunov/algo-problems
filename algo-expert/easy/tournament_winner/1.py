99426af91eca29d7c767798062cf7d30970a3834
Daniil Gorbunov <17042755+daniil-gorbunov@users.noreply.github.com>
2022-08-31 22:59:18 -0700

    tournament-winner solution added #easy #arrays

---
+++ algo-expert/easy/tournament_winner/1.py
@@ -0,0 +1,11 @@
+# easy, arrays
+# time O(n) | space O(t) n - competitions num, k - teams num
+def tournamentWinner(competitions, results):
+    best = competitions[0][0]
+    scores = {best: 0}
+    for i, comp in enumerate(competitions):
+        winner = comp[results[i] - 1]
+        scores[winner] = result_map.setdefault(winner, 0) + 3
+        if scores[winner] > scores[best]:
+            best = winner
+    return best
