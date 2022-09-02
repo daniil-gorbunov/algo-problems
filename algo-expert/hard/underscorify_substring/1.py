bec52cabe660b6feaceec0672af7dee0e616d9ca
Daniil Gorbunov <17042755+daniil-gorbunov@users.noreply.github.com>
2022-09-02 09:27:04 -0700

    underscorify-substring minor refactoring

--- algo-expert/hard/underscorify_substring/1.py
+++ algo-expert/hard/underscorify_substring/1.py
@@ -1,12 +1,12 @@
 # hard, strings
-# time O(n*m) | space O(n)
+# time O(n^2 + n*m) | space O(n)
 # n - string length, m - substring length
 def underscorifySubstring(str, substr):
     stringParts = []
     rangeEnd = -1
     for i in range(len(str) + 1):
         char = str[i] if i < len(str) else ''
-        if isStartOfSubstr(i, str, substr):
+        if str.find(substr, i) == i:
             if i > rangeEnd:
                 stringParts.append('_')
             rangeEnd = i + len(substr)
@@ -14,9 +14,3 @@ def underscorifySubstring(str, substr):
             stringParts.append('_')
         stringParts.append(char)
     return ''.join(stringParts)
-
-def isStartOfSubstr(i, str, substr):
-    for j in range(len(substr)):
-        if i + j >= len(str) or str[i + j] != substr[j]:
-            return False
-    return True
